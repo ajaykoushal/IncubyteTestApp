35f9f92e3e262124f49f2fbec5ceac6fff5ebe6e
Ajay Koushal <ajaykoushal6878@gmail.com>
2024-09-05 13:20:57 +0530

    Improve process of string and split add method

--- string_calculator/views.py
+++ string_calculator/views.py
@@ -2,57 +2,33 @@ from django.shortcuts import render
 
 
 class StringCalculator:
-    
     def _process_numbers(self, numbers: str):
         if not numbers:
             return []
-
-        # Strip leading and trailing spaces
-        numbers = numbers.strip()
         
-        # remove additional quotes from string if there any
-        if "'" in numbers:
-            numbers = numbers.strip("'")
-        elif '"' in numbers:
-            numbers = numbers.strip('"')
-
+        numbers = numbers.strip().strip("'\"")
 
         # Handle custom delimiters
         if numbers.startswith('//'):
             delimiter_end = numbers.find('\n')
             if delimiter_end != -1:
                 custom_delimiter = numbers[2:delimiter_end].strip()
-                numbers = numbers[delimiter_end+1:]
-                # Replace all occurrences of the custom delimiter and newline characters with a comma
-                numbers = numbers.replace(custom_delimiter, ',')
+                numbers = numbers[delimiter_end+1:].replace(custom_delimiter, ',')
         else:
-            # Replace default delimiters (comma and newline) with a comma
             numbers = numbers.replace('\\n', '\n').replace('\n', ',')
         
-        # Replace all delimiters with a comma
-        numbers = numbers.replace(',', ',')
-        
-        # Split the string by comma and strip any extra spaces
-        number_list = numbers.split(',')
-        number_list = [num.strip() for num in number_list if num.strip()]
+        return [num.strip() for num in numbers.split(',') if num.strip()]
 
-        return number_list
-
-    def add(self, numbers: str) -> int:
+    def _calculate(self, numbers):
         number_list = self._process_numbers(numbers)
-        total = 0
-        negatives = []
-
-        for num in number_list:
-            try:
-                n = int(num)
-                if n < 0:
-                    negatives.append(n)
-                total += n
-            except ValueError:
-                raise ValueError("Invalid input")
+        negatives = [int(num) for num in number_list if int(num) < 0]
 
         if negatives:
             raise Exception(f"Negative numbers not allowed: {', '.join(map(str, negatives))}")
+        
+        if not number_list:
+            return 0
+        return sum(map(int, number_list))
 
-        return total
+    def add(self, numbers: str) -> int:
+        return self._calculate(numbers)
